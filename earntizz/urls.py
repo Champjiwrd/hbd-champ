bcf8ef843d3f8cdda4eb26746764878bcf9534ed
Warodom <warodom@mm.co.th>
2022-01-10 23:19:10 +0700

    update covid

--- earntizz/urls.py
+++ earntizz/urls.py
@@ -20,7 +20,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('<name>', views.index,name='home-page'),
+    path('', views.index,name='home-page'),
     # path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
 
 ]
--- earntizz/views.py
+++ earntizz/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # @login_required
-def index(request,name):
-    print('test')
-    return render(request,'index.html',{'name':name})
+def index(request,):
+    return render(request,'index.html')
